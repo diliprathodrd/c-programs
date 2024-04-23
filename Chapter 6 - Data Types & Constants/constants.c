42ae1f231cf637bd3b5eb40567af1966653bc3fc
diliprathodrd <diliprathodofficial@gmail.com>
2024-04-23 21:46:54 +0530

    chapter 6 - data types and constants

---
+++ Chapter 6 - Data Types & Constants/constants.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define PI 3.14159
+#define MAX_SIZE 100
+
+int main() {
+    // Constants
+    const int LENGTH = 10;
+    const char NEWLINE = '\n';
+    
+    // Output
+    printf("PI: %.2f\n", PI);
+    printf("MAX_SIZE: %d\n", MAX_SIZE);
+    printf("NEWLINE: %c", NEWLINE);
+    printf("LENGTH: %d\n", LENGTH);
+    
+    return 0;
+}
---
+++ Chapter 6 - Data Types & Constants/dataTypes.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // Integer data types
+    int integerVariable = 10;
+    short shortVariable = 5;
+    long longVariable = 1234567890;
+    unsigned int unsignedInteger = 100;
+    
+    // Floating-point data types
+    float floatVariable = 3.14;
+    double doubleVariable = 3.14159;
+    
+    // Character data type
+    char charVariable = 'A';
+    
+    // Output
+    printf("Integer: %d\n", integerVariable);
+    printf("Short: %d\n", shortVariable);
+    printf("Long: %ld\n", longVariable);
+    printf("Unsigned Integer: %u\n", unsignedInteger);
+    printf("Float: %f\n", floatVariable);
+    printf("Double: %lf\n", doubleVariable);
+    printf("Character: %c\n", charVariable);
+    
+    return 0;
+}
